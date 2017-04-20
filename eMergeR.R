d95eabd7c2fd03b7732a59a9c00c334408a911d5
AWKruijt <mail@awkruijt.nl>
2017-04-20 18:44:35 +0100

    Update eMergeR.R
    Seems like I had been a tad too hasty when adding the file safe option. Removing it for now, as it was more of an extra bob anyways. :)

--- eMergeR.R
+++ eMergeR.R
@@ -4,33 +4,32 @@
 
 # The eMergeR function searches the currently set working directory for all files which names contain the string(s) specified in the 'filenamecontains' argument, 
 # parses them using ahebrank's edatparser function, adds header-info (Subject, Session, etc) and eventually returns a dataframe with all the files parsed and merged.
-# Optionally, the output dataframe can be saved (in the working directory) as a .csv file.
 
 # the default value of the filenamecontains argument is set to ".txt" so that calling eMergeR() tells the function to simply merge all .txt files found in the currently set working directory. 
   
 
-eMergeR <- function(filenameContains = ".txt", saveFile = F, fileName = "eMergeRd.csv") {
-    
-    if(!require(edatparser)){
-      devtools::install_github('ahebrank/edatparser')
-      library(edatparser)
-    }
-    
-    files <- list.files(pattern = filenameContains)
-    
-    do.call(rbind,lapply(files, function(x) {
-      e <- edat(x)
-      
-      header <- as.data.frame(t(e$header_info))
-      names(header)
-      headercut <- header[,which(names(header) == "SessionDate"):length(names(header))]
-      
-      dat <- as.data.frame(e)
-      
-      dat <- cbind(headercut, dat) }) )
-    
-    if (saveFile == T ){  write.csv(df, fileName)}
+eMergeR <- function(filenameContains = ".txt") {
+  
+  if(!require(edatparser)){
+    devtools::install_github('ahebrank/edatparser')
+    library(edatparser)
   }
+  
+  files <- list.files(pattern = filenameContains)
+  
+  do.call(rbind.fill,lapply(files, function(x) {
+    e <- edat(x)
+    
+    header <- as.data.frame(t(e$header_info))
+    names(header)
+    headercut <- header[,which(names(header) == "SessionDate"):length(names(header))]
+    
+    dat <- as.data.frame(e)
+    
+    dat <- cbind(headercut, dat) }) )
+  
+}
+
 
 # examples: 
 
@@ -41,7 +40,3 @@ df <- eMergeR("VAS") # merge all files for which the filename contains "VAS"
 
 df <- eMergeR(glob2rx("*VAS*.txt")) # merge all files for which the filename contains both "VAS" and ".txt" (using glob2rx to construct reg expr, * is a wildcard)
 
-
-eMergeR(glob2rx("*VAS*.txt"), saveFile = T, fileName = "othernamethandefault.csv") # merge files with "VAS" and ".txt" in their name and safe results as .csv with set name.
-
- 
